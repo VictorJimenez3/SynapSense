6f84d602627586041c9ad62899dbd05ebbf2c7da
Binimal101 <matthewtujague@gmail.com>
2025-02-02 17:00:40 +0000

    Last commit

--- frontend/src/actions/DatabaseActions.ts
+++ frontend/src/actions/DatabaseActions.ts
@@ -1,15 +1,9 @@
-import sqlite3 from 'sqlite'
-import { open } from 'sqlite'
+"use server"
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database("~/SynapSense/network_logger.db")
 
-export async function openDb () {
-    return open({
-      filename: '~/project/synapsense/network_logger.db',
-      driver: sqlite3.Database
-    })
-}
 
 export async function getRow() {
-    const db = await openDb()
-    const result = await db.get("SELECT * FROM ")
-    console.log(result)
+    const row = await db.get("SELECT * FROM table")
+    console.log(row)
 }
