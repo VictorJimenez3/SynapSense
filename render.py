2e6800b4a3883baa83197c639e0d35c3689652e2
Binimal101 <matthewtujague@gmail.com>
2025-02-02 04:17:00 +0000

    not saving to file; registered in html variable

--- render.py
+++ render.py
@@ -1,10 +1,26 @@
 from mitmproxy import http
 
-def response(flow: http.HTTPFlow, number = []):
-    if not len(number):
-        number = 0
+def response(flow: http.HTTPFlow):
     
-    number += 1
+    if flow.response and flow.response.headers:
+        
+        if "text/html" in flow.response.headers.get("content-type", ""):
+            html = flow.response.content if flow.response.content else None
+            url = flow.request.pretty_url if flow.request.pretty_url else None
+            print(f"I AM AN HTML FILE")
 
-    with open(f"{number[0]}.html", "wb") as f:
-        f.write(flow.response.raw_content)
+            if html and b"<!DOCTYPE html>" in html: #some text/html files aren't actual html files :(, TRICKERY!
+                print(f"good html content {type(html)} {len(html)}, URL: {url}")
+                with open("website.html", "w") as f:
+                    f.write("HELLOOOO")
+                    file.close()
+        elif flow.request.pretty_url: 
+            url = flow.request.pretty_url if flow.request.pretty_url else None
+            if "/short" in url:
+                print(f"{flow.response.headers.get('content-type', '')} on URL: {url}")
+        else:
+            try:
+                print(f'{flow.response.headers.get("content-type", "")} on URL: {flow.request.url}')
+            except:
+                pass
+
